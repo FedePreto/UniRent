068bb4ddff3b81b2105a05a223ca7ecc7f8ee1f6
Nicolò <s1092919@studenti.univpm.it>
2022-05-18 12:03:09 +0200

    aggiunto qualche annuncio per vedere come si comporta la pagina

--- resources/views/layout/catalog.blade.php
+++ resources/views/layout/catalog.blade.php
@@ -1,21 +1,34 @@
-<p class="w3-margin">Annunci trovati: <b>50</b></p>
+@isset($alloggi)
+@php 
+$num_pages = count($alloggi)/6;
+$pages = array();
+$counter = 0;
+for($i = 0; $i < count($alloggi); $i++){
+    $pages[$counter][$i] = $alloggi[$i];
+    
+    if($counter%3==0){
+        $counter++;
+    }
+}
+
+@endphp
+<p class="w3-margin">Annunci trovati: <b>@php echo count($alloggi) @endphp</b></p>
 <div class="w3-row-padding">
-    @isset($alloggi)
     @foreach ( $alloggi as $alloggio)
     <div class="w3-third w3-container w3-margin-bottom">
         
         
         <img src="{{asset('/img/appartamenti/20210505102417-4.jpg')}}" alt="Norway" style="width:100%" class="w3-hover-opacity">
         <div class="w3-container w3-white">
-            <p class="price"><b>{{ $alloggio->prezzo }}</b></p>
+            <p class="price"><b>€{{ $alloggio->prezzo }}</b></p>
             <p><b>{{$alloggio->titolo}}</b></p>
             <p>{{ $alloggio->descrizione }}</p>
         </div>
     </div>
     @endforeach
-    @endisset
     
 </div>
+@endisset
 
 <!-- 
 <div class="w3-center w3-padding-32">

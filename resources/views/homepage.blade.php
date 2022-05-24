fb5e783b223a187105bc735f8b398af427248e03
S1092514 <s1092514@studenti.univpm.it>
2022-05-24 11:01:12 +0200

    Update homepage.blade.php

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -83,9 +83,11 @@
   </div> 
   
 <!-- Image of location/map -->
-<div class="w3-container w3-padding-32" id="where">
+<div  class="w3-container w3-padding-32" id="where">
     <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16"><b>Dove trovarci</b></h3>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.9626846079573!2d13.51172641514125!3d43.58649357912361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d8024b0b1a877%3A0x5fac49ae852fdea0!2sVia%20Brecce%20Bianche%2C%2012%2C%2060131%20Ancona%20AN!5e0!3m2!1sit!2sit!4v1653229189286!5m2!1sit!2sit" width="1500" height="500"></iframe>
+    <p align="center"> 
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2889.9626846079573!2d13.51172641514125!3d43.58649357912361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d8024b0b1a877%3A0x5fac49ae852fdea0!2sVia%20Brecce%20Bianche%2C%2012%2C%2060131%20Ancona%20AN!5e0!3m2!1sit!2sit!4v1653229189286!5m2!1sit!2sit" width="1200" height="700" align="center"></iframe>
+    </p>
 </div>
 
 

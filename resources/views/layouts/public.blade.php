4997d853453a55f84f344af80a883fdb72ae3e03
S1092514 <s1092514@studenti.univpm.it>
2022-05-25 12:15:45 +0200

    homepage sistemata

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -22,9 +22,13 @@
         </div>
 
         <!-- First Parallax Image with Logo Text -->
-        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home"></div>
+        <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
             <div class="w3-display-middle" style="white-space:nowrap;">
               <span class="w3-center w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">UniRent</span>
+            </div>
+            <br>
+            <div class="w3-display-middle" style="white-space:nowrap;">
+              <span class="logo w3-padding-large w3-black w3-xlarge w3-wide w3-animate-opacity">Cerca l'appartamento su misura per i tuoi studi</span>
             </div>
         </div>
 

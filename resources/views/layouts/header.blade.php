80d1de7d92acb2c90b2df81f3f020e867a833bf6
S1092514 <s1092514@studenti.univpm.it>
2022-05-27 11:15:24 +0200

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -10,12 +10,11 @@
           {!! Form::close() !!}
           <hr>
           
-          <button type="button" id="#btn-reveal" class="my-button w3-center">
+          <button type="button" id="btn-reveal" class="my-button w3-center">
             <img src="{{asset('img/right-arrow.png')}}" width="20px" class="profile-name arrow " id="profile-arrow" > Mostra filtri</button>
           <div class="w3-container wrapper" id="filtri">
               <div id="reveal-content" class='hide'>
-                             
-                <h3 style = "color:red">Effettua prima l'accesso per poter utilizzare i filtri</h3>
+                  <h3 style = "color:red">Effettua prima l'accesso per poter utilizzare i filtri</h3>
               </div>
           </div>
                 
@@ -28,8 +27,8 @@
                     <li>{{ Form::radio('tipo_camera','appartamento',isset($request) ? $request->tipo_camera == 'appartamento': false,array('form'=>'ricerca'))}}Appartamento</li>
                     <li>{{ Form::radio('tipo_camera','posto_letto', isset($request) ? $request->tipo_camera == 'post_letto' : false,array('form'=>'ricerca'))}}Posto Letto</li>
                   </ul>   
-    
                 </div>
+
                 <div class="my-align">
                   {{Form::label('Data inizio contratto:')}}<br>
                   {{Form::date('data_inizio',false,array('form'=>'ricerca'))}}<br><br>
@@ -48,17 +47,19 @@
                   
                   <li>{{Form::checkbox($servizio->nome,$servizio->id,isset($request) ? $request->only($servizio->nome)!=null : false,array('form'=>'ricerca'))}} {{Form::label($servizio->nome)}}</li>
                     @if($i%4==0 )
-                      </ul></div><div class="my-align"><ul class="w3-bar-block w3-text my-filter">
+                  </ul>
+                </div>
+                <div class="my-align">
+                  <ul class="w3-bar-block w3-text my-filter">
                     @endif
                     @php
                     $i++;
                     @endphp
                   @endforeach
-                </ul>
-              </div>
-            </div>
+                  </ul>
+                </div>
+            
             @endisset
-          </div>
         </div>      
       <hr>
 </header>

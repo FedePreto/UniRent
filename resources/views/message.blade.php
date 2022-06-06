89c18dd91ac0ae27239a6d8d92d9ac072e0d0d92
S1092514 <s1092514@studenti.univpm.it>
2022-06-06 16:49:35 +0200

    Update message.blade.php

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -63,6 +63,9 @@
 @section('content')
 <div>
     <div class="w3-row-padding">
+
+
+        <!-- Div delle chat a sinistra-->
         <div style="border: 1px solid rgb(221, 221, 221);  box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px; height:650px; float:left; width:19.5%;">
 
             @if(count($chat)>0)
@@ -89,37 +92,35 @@
             @else
             <div align="center" class="w3-padding"><span class="w3-opacity w3-large"><b>Non hai chat aperte.</b></span></div>
             @endif
-
-
         </div>
-        <div style="overflow:auto; border: 1px solid rgb(221, 221, 221); padding-right: 20px; padding-top: 0px ;padding-left: 20px;padding-bottom: 0px; box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px; height:650px; float:right; width:80%;">
+
+        <!-- Div delle conversazioni a destra-->
+        <div style="overflow:auto; border: 1px solid rgb(221, 221, 221);  padding-top: 0spx ;padding-bottom: 0px; box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px; height:650px; float:right; width:80%;">
             @if(isset($messaggi))
-            <div style="overflow:auto; border: 1px solid rgb(221, 221, 221); background-color:rgb(220, 220, 220); padding-right: 20px; padding-top: 10px ;padding-left: 20px;padding-bottom: 0px; box-shadow: rgba(0, 0, 0, 0.12) 0px 6px 16px; height:150px; float:top; width:100%;">
-                <h2><b>{{$messaggi["mittente"][0]["name"]." ".$messaggi["mittente"][0]["cognome"]}}</b></h2>
-                <h4><b>{{$messaggi["alloggio"][0]["titolo"].", ".$messaggi["alloggio"][0]["cap"].", ".$messaggi["alloggio"][0]["citta"].", ".$messaggi["alloggio"][0]["indirizzo"].", ".$messaggi["alloggio"][0]["numero"]}}</b></h4>
-                <h5>{{$messaggi["alloggio"][0]["descrizione"]}}</h5>
+            <div style="overflow:auto; border: 1px solid rgb(221, 221, 221); background-color:rgb(220, 220, 220); padding-right: 20px; padding-top: 5px ;padding-left: 20px;padding-bottom: 5px; height:150px; float:top; width:100%;">
+                <h3><b>{{$messaggi["mittente"][0]["name"]." ".$messaggi["mittente"][0]["cognome"]}}</b></h3>
+                <h5><b>{{$messaggi["alloggio"][0]["titolo"].", ".$messaggi["alloggio"][0]["cap"].", ".$messaggi["alloggio"][0]["citta"].", ".$messaggi["alloggio"][0]["indirizzo"].", ".$messaggi["alloggio"][0]["numero"]}}</b></h5>
+                <h6>{{$messaggi["alloggio"][0]["descrizione"]}}</h6>
             </div>
-            <div style="padding-top:10px;">
+            <div style="padding-top:0px;">
                 @foreach($messaggi["messaggi"] as $messaggio)
                 @if($messaggio["mittente"]==$id)
 
-                <div style="padding-left: 15px;">
-                    <div>
+                <div style="padding-left: 15px; padding-right: 15px; background-color:beige;">
+                    <div >
                         <h4 align="right"><b>{{$messaggio["contenuto"]}}</b></h4>
                         <h6 align="right">{{$messaggio["data"]}}</h6>
                     </div>
                 </div>
                 @else
-                <div style="padding-left: 15px;">
+                <div style="padding-left: 15px; padding-right: 15px;">
                     <div>
                         <h4 align="left"><b>{{$messaggio["contenuto"]}}</b></h4>
                         <h6 align="left">{{$messaggio["data"]}}</h6>
                     </div>
                 </div>
-
                 @endif
-                <br>
-                <hr>
+                
 
                 @endforeach
 

a9ec5a42fbf9cf86f906904fcbd2e6be0acdaedd
Gregorio Vecchiola <s1093425@studenti.univpm.it>
2022-06-07 11:24:00 +0100

    paginator style

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -6,32 +6,32 @@
 
         @if ($paginator->hasPages())
         <nav>
-        <ul class="pagination">
+        <div class="pagination">
             {{-- Previous Page Link --}}
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled w3-bar-item " aria-disabled="true" aria-label="@lang('pagination.previous')">
-                    <span class="page-link" aria-hidden="true">&lsaquo;</span>
-                </li>
+                <div class="page-item disabled w3-bar-item " aria-disabled="true" aria-label="@lang('pagination.previous')">
+                    <label class="page-link" aria-hidden="true">&lsaquo;</label>
+</div>
             @else
-                <li class="page-item w3-bar-item  ">
+                <div class="page-item w3-bar-item  ">
                     <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
-                </li>
+</div>
             @endif
 
             {{-- Pagination Elements --}}
             @foreach ($elements as $element)
                 {{-- "Three Dots" Separator --}}
                 @if (is_string($element))
-                    <li class="page-item disabled w3-bar-item  " aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    <div class="page-item disabled w3-bar-item  " aria-disabled="true"><label class="page-link">{{ $element }}</label></div>
                 @endif
 
                 {{-- Array Of Links --}}
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item activetor w3-bar-item  " aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                            <div class="page-item activetor w3-bar-item  " aria-current="page"><label class="page-link">{{ $page }}</label></div>
                         @else
-                            <li class="page-item w3-bar-item "><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
+                            <div class="page-item w3-bar-item "><a class="page-link" href="{{ $url }}">{{ $page }}</a></div>
                         @endif
                     @endforeach
                 @endif
@@ -39,15 +39,15 @@
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item w3-bar-item  ">
+                <div class="page-item w3-bar-item  ">
                     <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-                </li>
+</div>
             @else
-                <li class="page-item disabled w3-bar-item " aria-disabled="true" aria-label="@lang('pagination.next')">
-                    <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                </li>
+                <div class="page-item disabled w3-bar-item " aria-disabled="true" aria-label="@lang('pagination.next')">
+                    <label class="page-link" aria-hidden="true">&rsaquo;</label>
+</div>
             @endif
-        </ul>
+</div>
     </nav>
 @endif
     </div>
